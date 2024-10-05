78e693827ab33f59fdbab7bbeead6075f19fc9cb
Istiak <mhistiak2@gmail.com>
2024-10-06 03:14:21 +0600

    Ready code for production

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,12 @@
  *  node modules
  **/
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import path from "path";
+
 /**
  * custom modules
-**/
+ **/
 import { connectDB } from "./config/connectDB.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
@@ -21,15 +23,24 @@ import { CLIENT_URL } from "./config/index.js";
 
 // app object
 const app = express();
+const __dirname = path.resolve();
 
 // application level middleware
-app.use(cors({ origin: CLIENT_URL,credentials:true }));
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-
 // *  Application Routes
 app.use("/api/auth", authRouter);
+
+// serve static files
+
+if (process.env.NODE_ENV.trim() === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // start server
 const PORT = process.env.PORT || 3000;
