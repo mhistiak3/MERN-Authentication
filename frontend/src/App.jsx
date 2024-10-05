7ba9d0fbf8e406cec320e75aa460e19fce9d3de9
Istiak <mhistiak2@gmail.com>
2024-10-05 15:18:27 +0600

    Make Protected Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 
 import Home from "./pages/Home";
@@ -11,23 +11,43 @@ import { useAuthStore } from "./store/auth.store";
 import { useEffect } from "react";
 
 
+// Protected Routes
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, user,  } = useAuthStore();
+  
+ 
+  if (!isAuthenticated) {
+    return <Navigate to="/auth/login" />;
+  }
+  if (!user?.isVerified) {
+    return <Navigate to="/auth/login" />;
+  }
+  return children;
+};
 const App = () => {
-  const { isCheckeingAuth, checkAuth, isAuthenticated,user } = useAuthStore();
+  const { isCheckeingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
 
   useEffect(() => {
-    checkAuth()
+    checkAuth();
   }, [checkAuth]);
-  
+
   console.log("Is Checking Auth:", isCheckeingAuth);
   console.log("Is Auth:", isAuthenticated);
   console.log("User:", user);
-  
+
   return (
     <>
       <Routes>
         {/* Main Route */}
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/auth" element={<AuthLayout />}>
           {/* Child Routes */}
           <Route path="register" element={<Register />} />
@@ -35,7 +55,7 @@ const App = () => {
           <Route path="email-verify" element={<EmailVerification />} />
         </Route>
       </Routes>
-      <Toaster/>
+      <Toaster />
     </>
   );
 };
